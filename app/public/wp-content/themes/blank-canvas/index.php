d5d342c7ad044e17775068a05c3cd0bd2f7fc561
Steve Anderson <swanderson79@gmail.com>
2022-05-31 11:59:11 -0400

    committing for submission. Fun challenge. I have not worked with Ajax or pulling in external information into a wordpress site. I couldn't figure out how to use Ajax so I wanted to submit at least the content that was asked for in the requirements so I used a RSS feed.

--- app/public/wp-content/themes/blank-canvas/index.php
+++ app/public/wp-content/themes/blank-canvas/index.php
@@ -2,9 +2,9 @@
 			/* Start the Loop */
 			while ( have_posts() ) :
                 ?>
-<div>
-    <h1> <?php echo get_the_title(); ?></h1>
-    <div><?php the_post(); ?></div>
+<div class="main-content" style="text-align:center;">
+    <h1 style="text-align:center;"> <?php echo get_the_title(); ?></h1>
+    <div style="text-align:center;"><?php the_post(); ?></div>
     <?php
 				get_template_part( 'template-parts/content/content-singular' );
 				
